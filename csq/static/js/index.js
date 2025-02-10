2b55e31d6a97fbfb465234d4ef83b5bae8990670
Yeho Gwon <yeho.gwon@postech.ac.kr>
2025-02-10 13:59:10 +0900

    skip loading interpolation figures

--- csq/static/js/index.js
+++ csq/static/js/index.js
@@ -65,6 +65,7 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
+    /*
     preloadInterpolationImages();
 
     $('#interpolation-slider').on('input', function(event) {
@@ -72,6 +73,7 @@ $(document).ready(function() {
     });
     setInterpolationImage(0);
     $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    */
 
     bulmaSlider.attach();
 
